46ba3defa6a0ad03d98c12354446cb50f1525f5e
Arthur Dingemans <arthur@argu.co>
2020-04-10 09:13:37 +0200

    dexpod#9 Add initializer to register API endpoints

---
+++ config/initializers/api_endpoints.rb
@@ -0,0 +1,16 @@
+# frozen_string_literal: true
+
+endpoints_key = 'frontend.runtime.plain_endpoints'
+db = 0
+
+current_keys = Redis.new(db: db).lrange(endpoints_key, 0, -1)
+required_keys = %w[
+  /.well-known/webfinger
+  /.well-known/openid-configuration
+  /oauth/(.*)
+]
+
+(required_keys - current_keys).each do |key|
+  Rails.logger.info "Registering #{key} as API endpoint"
+  Redis.new(db: db).lpush(endpoints_key, key)
+end
