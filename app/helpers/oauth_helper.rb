faa3012fddce10694cc548c304db0b31dc365330
Arthur Dingemans <arthur@argu.co>
2020-07-24 09:59:03 +0200

    dexpod#9 Prevent double render

--- app/helpers/oauth_helper.rb
+++ app/helpers/oauth_helper.rb
@@ -62,7 +62,7 @@ module OauthHelper
   end
 
   def redirect_guests
-    return true if current_user && !current_user&.guest?
+    return true if (current_user && !current_user&.guest?) || response_body
 
     redirect_to(LinkedRails.iri(path: '/u/sign_in', query: {r: request.original_url}.to_param).to_s)
   end
