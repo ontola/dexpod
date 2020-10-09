bb499fad19d5ce891b4a770f616f4dd72468fae1
Joep Meindertsma <joep@argu.co>
2020-10-09 09:53:07 +0200

    dexpod#10 Use symbols for predicates

--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -6,13 +6,13 @@ class UserSerializer < LinkedSerializer
 
   attribute :display_name, predicate: NS::FOAF[:name]
 
-  attribute :password, predicate: NS::ARGU[:password], datatype: NS::ONTOLA['datatype/password'], if: method(:never)
+  attribute :password, predicate: NS::ARGU[:password], datatype: NS::ONTOLA[:'datatype/password'], if: method(:never)
   attribute :password_confirmation,
             predicate: NS::ARGU[:passwordConfirmation],
-            datatype: NS::ONTOLA['datatype/password'],
+            datatype: NS::ONTOLA[:'datatype/password'],
             if: method(:never)
   attribute :current_password,
             predicate: NS::ARGU[:currentPassword],
-            datatype: NS::ONTOLA['datatype/password'],
+            datatype: NS::ONTOLA[:'datatype/password'],
             if: method(:never)
 end
