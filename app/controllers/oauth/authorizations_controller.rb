0ac99112e40da8ef3c3ed363ed7cc01ce90a2d99
Arthur Dingemans <arthur@argu.co>
2020-11-09 16:31:27 +0100

    dexpods#1 Fix oauth authorization

--- app/controllers/oauth/authorizations_controller.rb
+++ app/controllers/oauth/authorizations_controller.rb
@@ -4,8 +4,18 @@ module Oauth
   class AuthorizationsController < Doorkeeper::AuthorizationsController
     before_action :redirect_guests
 
+    private
+
     def flash
       {}
+    end
+
+    def redirect_or_render(auth)
+      if auth.redirectable?
+        redirect_to auth.redirect_uri
+      else
+        render json: auth.body, status: auth.status
+      end
     end
   end
 end
