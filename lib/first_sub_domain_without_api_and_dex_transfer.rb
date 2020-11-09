51c491438cc41fa49c6adcb30bf5c7f0ae9b07f5
Arthur Dingemans <arthur@argu.co>
2020-11-09 15:40:37 +0100

    dexpods#1 Fix landingpage on staging

--- lib/first_sub_domain_without_api_and_dex_transfer.rb
+++ lib/first_sub_domain_without_api_and_dex_transfer.rb
@@ -5,7 +5,7 @@ require 'apartment/elevators/first_subdomain'
 
 class FirstSubDomainWithoutAPIAndDexTransfer < Apartment::Elevators::Generic
   def parse_tenant_name(request)
-    return if spi_request?(request)
+    return if spi_request?(request) || landing_page?(request)
     return 'dex_transfer' if dex_transfer_host?(request)
 
     Apartment::Elevators::FirstSubdomain.new(@app, @processor).parse_tenant_name(request)
@@ -13,6 +13,10 @@ class FirstSubDomainWithoutAPIAndDexTransfer < Apartment::Elevators::Generic
 
   def dex_transfer_host?(request)
     request.url.starts_with?("https://#{Rails.application.config.dex_transfer_host_name}/")
+  end
+
+  def landing_page?(request)
+    request.url.starts_with?("https://#{Rails.application.config.host_name}/")
   end
 
   def spi_request?(request)
