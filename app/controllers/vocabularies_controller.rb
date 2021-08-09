718cfda3cfc28b2f13c7509c1c092d536a138640
Thom van Kalkeren <thom@argu.co>
2021-08-09 09:17:07 +0200

    core#1 Rename VocabulariesController to OntologyController

--- app/controllers/vocabularies_controller.rb
+++
@@ -1,9 +0,0 @@
-# frozen_string_literal: true
-
-class VocabulariesController < LinkedRails::VocabulariesController
-  def vocab_graph
-    Rails.cache.fetch([::VERSION, 'ns_core'].join('.')) do
-      super
-    end
-  end
-end
