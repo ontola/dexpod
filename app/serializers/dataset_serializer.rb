6dea3c1a7b72f4e035311e69a30c7767125be162
Thom van Kalkeren <thom@argu.co>
2021-10-05 20:05:16 +0200

    core#12 Fix dataset serialisation

--- app/serializers/dataset_serializer.rb
+++ app/serializers/dataset_serializer.rb
@@ -10,9 +10,11 @@ class DatasetSerializer < LinkedSerializer
     RDF::URI('https://dexpods.eu')
   end
   attribute :themes, predicate: NS.dcat[:theme]
+  attribute :web_id, predicate: NS.donl[:authority] do |object|
+    RDF::URI(object.web_id.identifier)
+  end
 
   enum :license, predicate: NS.dc.license, options: EnumHelper.list_options('licenses')
 
-  has_one :web_id, predicate: NS.donl[:authority]
   has_many :distributions, predicate: NS.dcat[:distribution]
 end
