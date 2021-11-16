f472e190a99dbc7faca5debda8be66349f254f46
Arthur Dingemans <arthur@argu.co>
2021-11-16 14:57:51 +0100

    dexpods#93 Change dex namespace

--- config/initializers/ns.rb
+++ config/initializers/ns.rb
@@ -7,7 +7,7 @@ class NS < LinkedRails::Vocab
   register_strict(dc)
   register_strict(org)
   register(:argu, 'https://argu.co/ns/core#')
-  register(:dex, 'https://argu.co/ns/dex#')
+  register(:dex, 'https://dexpods.eu/ns/core#')
   register(:dcat, 'http://www.w3.org/ns/dcat#')
   register(:donl, 'https://data.overheid.nl/dcat.rdf#')
 
