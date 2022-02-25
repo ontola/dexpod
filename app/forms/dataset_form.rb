62278266d1872916b6aeb468e3a78faf74c4b277
Arthur Dingemans <arthur@argu.co>
2022-02-25 10:56:00 +0100

    dexpods#116 Change form order

--- app/forms/dataset_form.rb
+++ app/forms/dataset_form.rb
@@ -10,36 +10,40 @@ class DatasetForm < ApplicationForm
       )
     ]
   end
-  field :title
-  field :description
-  field :dataspace_id,
-        sh_in: -> { Dataspace.collection_iri }
-  field :data_owned,
-        input_field: LinkedRails::Form::Field::CheckboxInput
-  field :data_owner,
-        min_count: 1
-  has_one :distributions,
-          label: '',
+
+  group :describe_data,
+        label: -> { I18n.t('forms.datasets.describe_data.label') },
+        collapsible: false do
+    field :title
+    field :description
+    has_one :distributions,
+            label: '',
+            min_count: 1,
+            if: with_part_of(has: true)
+    has_many :distributions,
+             min_count: 1,
+             if: with_part_of(has: false)
+    field :data_owned,
+          input_field: LinkedRails::Form::Field::CheckboxInput
+    field :data_owner,
+          min_count: 1
+    field :themes,
+          input_field: LinkedRails::Form::Field::SelectInput,
+          grouped: true,
+          datatype: NS.xsd.string,
+          sh_in: form_options_iri(:theme, DatasetTheme),
           min_count: 1,
-          if: with_part_of(has: true)
-  has_many :distributions,
-           min_count: 1,
-           if: with_part_of(has: false)
-  field :themes,
-        input_field: LinkedRails::Form::Field::SelectInput,
-        grouped: true,
-        datatype: NS.xsd.string,
-        sh_in: form_options_iri(:theme, DatasetTheme),
-        min_count: 1,
-        max_count: 99
+          max_count: 99
+  end
   group :conditions,
-        description: -> { I18n.t('forms.datasets.conditions.description') },
         label: -> { I18n.t('forms.datasets.conditions.label') },
         collapsible: false do
+    # field :dataspace_id,
+    #       sh_in: -> { Dataspace.collection_iri }
+    field :license,
+          min_count: 1
     Condition.types.each do |klass|
       has_one klass.name.underscore.to_sym
     end
   end
-  field :license,
-        min_count: 1
 end
